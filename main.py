5953b797f94592df07e17a764fa1b9df81be88bd
Winson Gin <winsongin911@gmail.com>
2021-06-02 21:35:24 -0700

    Wrote database configuration

--- main.py
+++ main.py
@@ -1,9 +1,45 @@
 from flask import Flask, jsonify, request
 from dotenv import load_dotenv
+from configparser import ConfigParser
+import mysql.connector
 
 load_dotenv() # Load environment variables from .env file
 
 app = Flask(__name__)
+
+# Reading MySQL credentials from .ini file
+config = ConfigParser()
+config.read('database.ini')
+
+HOST = config['mysql']['host']
+USER = config['mysql']['user']
+PASSWORD = config['mysql']['password']
+
+myDB = mysql.connector.connect(
+    HOST, 
+    USER,
+    PASSWORD
+)
+
+databaseCursor = myDB.cursor() 
+
+databaseCursor.execute('CREATE DATABASE IF NOT EXISTS FastRoute')
+databaseCursor.execute('CREATE TABLE IF NOT EXISTS inventory (
+    date DATE PRIMARY KEY NOT NULL,
+    product VARCHAR(255) NOT NULL, 
+    barcode INT NOT NULL,
+    location VARCHAR(255) NOT NULL,
+    aisle VARCHAR(255) NOT NULL
+)')
+databaseCursor.execute('CREATE TABLE IF NOT EXISTS onlineOrders (
+    batch INT PRIMARY KEY NOT NULL, 
+    date DATE NOT NULL,
+    product VARCHAR(255) NOT NULL, 
+    barcode INT NOT NULL, 
+    location VARCHAR(255) NOT NULL, 
+    aisle VARCHAR(255) NOT NULL
+    purpose VARCHAR(255) NOT NULL
+)')
 
 # Handle 404 Not Found
 @app.errorhandler(404)
@@ -36,14 +72,46 @@ def backstock_product():
     parameters = request.get_json(force=True) 
     
     product = parameters['product'] 
-    barcodeNum = parameters['barcode']
+    barcode = parameters['barcode']
+    date = parameters['date']
+    
+    
+@app.route('/api/v1.0/inventory/pull', methods=['PUT'])
+def pull_product(): 
+
+    parameters = request.get_json(force=True) 
+
+    product = parameters['product']
+    barcode = parameters['barcode']
+    location = parameters['location']
     date = parameters['date']
 
-    # Need to somehow determine backstock locations
-    
-# @app.route('api/v1.0/inventory/pull', methods=['PUT'])
-# def pull_product(): 
-#     pass
+# OPUs - Order Pick-Ups
+@app.route('/api/v1.0/OPUs')
+def picks_for_OPUs():
+
+    parameters = request.get_json(force=True)
+
+    batch = parameters['batch']
+    product = parameters['product']
+    barcode = parameters['barcode']
+    location = parameters['location']
+    date = parameters['date'] 
+    purpose = "OPUs"
+
+# Pick items for SFS (Ship From Store) batches
+@app.route('api/v1.0/SFS')
+def picks_for_SFS():
+
+    parameters = request.get_json(force=True)
+
+    batch = parameters['batch']
+    product = parameters['product']
+    barcode = parameters['barcode']
+    location = parameters['location']
+    date = parameters['date']
+    purpose = "SFS"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
