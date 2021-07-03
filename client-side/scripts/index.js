e8d394278911c6d5b0f0aed57a32dc4a7a034f48
Winson Gin <winsongin911@gmail.com>
2021-07-02 22:34:30 -0700

    Initial commit

---
+++ client-side/scripts/index.js
@@ -0,0 +1,31 @@
+"use strict";
+
+let customer = document.getElementById("customer-name");
+let orderNumber = document.getElementById("order-number");
+let scanItemButton = document.getElementById("scan-location");
+
+let goBacks = document.getElementById("go-backs");
+goBacks.addEventListener("click", function () {
+
+  customer.classList.add("toggle");
+  orderNumber.classList.add("toggle");
+});
+
+let orderPickUps = document.getElementById("order-pick-ups");
+orderPickUps.addEventListener("click", function () {
+  customer.classList.remove("toggle");
+  orderNumber.classList.remove("toggle");
+});
+
+let shipFromStore = document.getElementById("ship-from-store");
+shipFromStore.addEventListener("click", function () {
+  // Replace the text for the scan button
+  while (scanItemButton.firstChild) {
+    scanItemButton.removeChild(scanItemButton.firstChild);
+  }
+  scanItemButton.appendChild(document.createTextNode("Scan Product"));
+
+  // Toggle off the Customer Name and Order #
+  customer.classList.remove("toggle");
+  orderNumber.classList.remove("toggle");
+});
