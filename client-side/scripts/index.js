7d469d9160525101ccb27ea358d1ec5a7db5b82e
Winson Gin <winsongin911@gmail.com>
2021-07-04 21:04:02 -0700

    Added Scan Product button to OPUs

--- client-side/scripts/index.js
+++ client-side/scripts/index.js
@@ -6,13 +6,23 @@ let scanItemButton = document.getElementById("scan-location");
 
 let goBacks = document.getElementById("go-backs");
 goBacks.addEventListener("click", function () {
-
+  while (scanItemButton.firstChild) {
+    scanItemButton.removeChild(scanItemButton.firstChild);
+  }
+  scanItemButton.appendChild(document.createTextNode("Scan Location"));
+  // Toggle off Customer Name and Order Number
   customer.classList.add("toggle");
   orderNumber.classList.add("toggle");
 });
 
 let orderPickUps = document.getElementById("order-pick-ups");
 orderPickUps.addEventListener("click", function () {
+  while (scanItemButton.firstChild) {
+    scanItemButton.removeChild(scanItemButton.firstChild);
+  }
+  scanItemButton.appendChild(document.createTextNode("Scan Product"));
+
+  // Toggle off Customer Name and Order Number
   customer.classList.remove("toggle");
   orderNumber.classList.remove("toggle");
 });
